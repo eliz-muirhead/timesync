8a893bed0edb65a43df39702d20761395eec2353
elizabeth <eliz.muirhead@gmail.com>
2023-10-02 20:39:37 -0400

    cleanup

--- TSServer.py
+++ TSServer.py
@@ -2,10 +2,10 @@ import socket
 import time
 
 """
-This class initializes a local server that can establish
+This class initializes a socket server that can establish
 a TCP connections with clients. It listens for incoming requests,
 timestamps the reception of the request, then timestamps and
-sends responds to clients.
+sends the response to the clients.
 
 Usage:
 python TSServer.py
@@ -16,12 +16,12 @@ class TSServer:
 
     def __init__(self):
         self.serverPort = 10085
-        self.my_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.my_server_socket.bind(('', self.serverPort))
-        self.my_server_socket.listen(1)
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind(('', self.serverPort))
+        self.server_socket.listen(1)
 
     def send_response(self):
-        connection_socket, addr = self.my_server_socket.accept()
+        connection_socket, addr = self.server_socket.accept()
         # t2 measured at the time the request is received
         t2 = time.time().__str__()
 
@@ -30,10 +30,7 @@ class TSServer:
         connection_socket.close()
 
 
-def main():
+if __name__ == "__main__":
     server = TSServer()
     while True:
         server.send_response()
-
-
-main()
